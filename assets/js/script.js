f666b859a547eb6841e42d3925de77e26899cb38
gomathishankar28 <gomathishankar28@gmail.com>
2022-01-11 20:18:54 +0000

    created adduser modal, getformdata function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-function adduser(form) {
-    event.
+function onFormSubmit() {
+    var formData = getFormData();
+    createUSerCard();
+}
+
+function getFormData() {
+    var formData = {};
+    formData["emp_no"] = document.getElementById("emp_no").value
+    formData["fullname"] = document.getElementById("fname").value + " " + document.getElementById("fname").value
+    formData["mobile"] = document.getElementById("mobile").value
+    formData["emaail"] = document.getElementById("email").value
+    return formData;
+}
+
+function createUSerCard() {
+    
 }
