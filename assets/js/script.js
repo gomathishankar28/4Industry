ddd504e57e5af73324c1df2fea3b608ad241296b
gomathishankar28 <gomathishankar28@gmail.com>
2022-01-11 21:46:49 +0000

    created renderusercards function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,44 @@
+var userData = [ 
+    {  
+        "emp_no":"10001",
+        "fullname":"gomathi shankar",
+        "mobile":"9600048330",
+        "email": "gomsy@gmail.com"
+     },
+     {  
+        "emp_no":"10002",
+        "fullname":"gomathi shankar",
+        "mobile":"9600048330",
+        "email": "gomsy@gmail.com"
+     },
+     {  
+        "emp_no":"10003",
+        "fullname":"gomathi shankar",
+        "mobile":"9600048330",
+        "email": "gomsy@gmail.com"
+     },
+     {  
+        "emp_no":"10004",
+        "fullname":"gomathi shankar",
+        "mobile":"9600048330",
+        "email": "gomsy@gmail.com"
+     },
+]
+
+function renderUSerCards() {
+    for(var i = 0; i < userData.length; i++){
+        var cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+        var emp_no = document.createElement('p');
+        emp_no.className = 'emp_no';
+        emp_no.innerHTML = userData[i].emp_no;
+        cardDiv.append(emp_no); 
+        console.log(document.getElementsByClassName("usercards").childNode[0]);
+    }
+    
+}
 function onFormSubmit() {
     var formData = getFormData();
-    createUSerCard();
 }
 
 function getFormData() {
@@ -13,5 +51,5 @@ function getFormData() {
 }
 
 function createUSerCard() {
-    
+
 }
