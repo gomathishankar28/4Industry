c6a3be54abe97e8bed6bc05a562e30a69b564f1a
gomathishankar28 <gomathishankar28@gmail.com>
2022-01-16 21:59:03 +0000

    form validation done

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,10 +25,11 @@ var userData = [
      },
 ]
 
-if (localStorage.get("userData") == null){
+if(localStorage.get("userData")  == null) {
 var userdata_str = JSON.stringify(userData);
 localStorage.setItem("userData", userdata_str); 
 }
+
 function renderUSerCards() {
     let userDatas = JSON.parse(localStorage.getItem("userData"));
     console.log(userDatas);
@@ -49,7 +50,7 @@ function renderUSerCards() {
             emp_no.innerHTML = userDatas[i].emp_no;
             var fullname = document.createElement('p');
             fullname.className = 'fullname';
-            fullname.innerHTML = userDatas[i].fullname;
+            fullname.innerHTML = userDatas[i].fullname.toUpperCase();
             var mobile = document.createElement('p');
             mobile.className = 'mobile';
             mobile.innerHTML = userDatas[i].mobile;
@@ -58,9 +59,10 @@ function renderUSerCards() {
             email.innerHTML = userDatas[i].email;
             var btns = document.createElement('p');
             btns.className = 'btn'
-            btns.innerHTML = `<a data-toggle="modal" href="#edituser" onclick="return editUSerCard(this)">EDIT</a>
-            <a onclick="deleteUserCard(this)">DELETE</a>`;
-            cardDiv.append(badges, emp_no, fullname, mobile, email, btns); 
+            btns.innerHTML = `<a data-toggle="modal" href="#edituser" onclick="return editUSerCard(this)">
+            <i class="fas fa-edit"></i></a>
+            <a onclick="deleteUserCard(this)"><i class="fas fa-trash-alt"></i></a>`;
+            cardDiv.append(badges, fullname, emp_no, mobile, email, btns); 
             console.log(cardDiv);
             mainDiv.appendChild(colDiv);
         }
@@ -79,6 +81,15 @@ function onFormSubmit() {
 }
 function getFormData() {
     var formData = {};
+    var oldItems = JSON.parse(localStorage.getItem('userData'));
+    for (var i=0; i<oldItems.length; i++) {
+        if ( document.getElementById("emp_no").value == oldItems[i].emp_no) {
+            document.getElementById("emp_no").style.border = "2px solid red";
+        return alert("Employee number already exists.");
+        break; 
+            
+        }
+    }
     formData["emp_no"] = document.getElementById("emp_no").value;
     formData["fullname"] = document.getElementById("fname").value + " " + document.getElementById("lname").value;
     formData["mobile"] = document.getElementById("mobile").value;
@@ -106,13 +117,13 @@ function createUSerCard(formData) {
         badges.className = 'badge';
         var badgeText = formData.fullname.split(" ")[0].slice(0,1).toUpperCase() + formData.fullname.split(" ")[1].slice(0,1).toUpperCase();
         badges.innerHTML = badgeText;
+        var fullname = document.createElement('p');
+        fullname.className = 'fullname';
+        fullname.innerHTML = formData.fullname.toUpperCase();
         var emp_no = document.createElement('p');
         emp_no.className = 'emp_no';
         emp_no.innerHTML = formData.emp_no;
-        var fullname = document.createElement('p');
-        fullname.className = 'fullname';
-        fullname.innerHTML = formData.fullname;
-        var mobile = document.createElement('p');
+       var mobile = document.createElement('p');
         mobile.className = 'mobile';
         mobile.innerHTML = formData.mobile;
         var email = document.createElement('p');
@@ -120,9 +131,9 @@ function createUSerCard(formData) {
         email.innerHTML = formData.email;
         var btns = document.createElement('p');
         btns.className = 'btn'
-        btns.innerHTML = `<a data-toggle="modal" href="#edituser" onclick="return editUSerCard(this)">EDIT</a>
-        <a onclick="deleteUserCard(this)">DELETE</a>`;
-        cardDiv.append(badges, emp_no, fullname, mobile, email, btns);
+        btns.innerHTML = `<a data-toggle="modal" href="#edituser" onclick="return editUSerCard(this)"><i class="fas fa-edit"></i></a>
+        <a onclick="deleteUserCard(this)"><i class="fas fa-trash-alt"></i></a>`;
+        cardDiv.append(badges, fullname, emp_no, mobile, email, btns);
         console.log(cardDiv);
         mainDiv.appendChild(colDiv);
 }
@@ -132,9 +143,6 @@ function onEditFormSubmit() {
     console.log(formData);
     updateUSerCard(formData);
     window.location.reload();
-    if (confirm("USer is updated successfully?")) {
-        document.getElementById('btn-view').click;
-    } 
 }
 function getEditFormData() {
     var formData = {};
@@ -147,11 +155,11 @@ function getEditFormData() {
 
 function editUSerCard(e) {
     var selected_card = e.parentElement.parentElement;
-    document.getElementById("efname").value = selected_card.childNodes[2].innerHTML.split(" ")[0];
-    document.getElementById("elname").value = selected_card.childNodes[2].innerHTML.split(" ")[1];
+    document.getElementById("efname").value = selected_card.childNodes[1].innerHTML.split(" ")[0];
+    document.getElementById("elname").value = selected_card.childNodes[1].innerHTML.split(" ")[1];
     document.getElementById("emobile").value = selected_card.childNodes[3].innerHTML;
     document.getElementById("eemail").value = selected_card.childNodes[4].innerHTML;
-    document.getElementById("eemp_no").value = selected_card.childNodes[1].innerHTML;
+    document.getElementById("eemp_no").value = selected_card.childNodes[2].innerHTML;
     
  }
 
@@ -189,7 +197,7 @@ function deleteUserCard(usercard) {
     if (confirm("Are you sure you want to delete this user?")) {
         usercard.parentElement.parentElement.remove();
         var oldItems = JSON.parse(localStorage.getItem('userData'));
-        var delcardno = usercard.parentElement.parentElement.childNodes[1].innerHTML;
+        var delcardno = usercard.parentElement.parentElement.childNodes[2].innerHTML;
         let count = JSON.parse(localStorage.getItem("userData")).length;
         var index;
         for(var i=0; i < count; i++) {
@@ -197,8 +205,10 @@ function deleteUserCard(usercard) {
                 index = i;
                 break;
             }
-        }  
-        oldItems.splice(index, 1);
+        } 
+        newItem = oldItems.splice(index, 1);
+        
+        /*console.log(oldItems.splice(index, 1));*/
         console.log(oldItems);
     }
     localStorage.setItem("userData", JSON.stringify(oldItems));
